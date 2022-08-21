5c0fd58614348bd96c6760d241e7bed15436d23b
Dwi Kumara Widyatna <dwi@dinamika.ac.id>
2022-08-21 20:51:24 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/18410100192.jpg" alt="Dwi Kumara Widyatna" width="200" class="img-thumbnail">
+    <p>Halo, nama saya <?= $data['nama']; ?> dan saya adalah seorang <?= $data['pekerjaan']; ?>  </p>
+</div>
